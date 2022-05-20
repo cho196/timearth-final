e51fcbea3a2c1c1f1ed8a7b7110518abb7864ca7
cho196 <cho196@my.bcit.ca>
2022-05-20 08:00:44 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ timearth/data/intro_content.js
@@ -0,0 +1,33 @@
+export const PageTitle = ["Let's Learn About Earth Minutes!", "Let’s Start!"]
+
+export const BtnText = ["Next", "Start Quiz"]
+
+export const cat1 = {
+    title: "Transportation",
+    img: "/Transportation.svg"
+}
+
+export const cat2 = {
+    title: "Food",
+    img: "/Food.svg"
+}
+
+export const cat3 = {
+    title: "Water",
+    img: "/Water.svg"
+}
+
+export const cat4 = {
+    title: "Electricity",
+    img: "/Electricity.svg"
+}
+
+export const cat5 = {
+    title: "Waste",
+    img: "/Waste.svg"
+}
+
+export const cat6 = {
+    title: "Shopping",
+    img: "/Shopping.svg"
+}
